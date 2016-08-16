ff1c629261886b15398b8999c3489298d8c2b28a
Rebeca Alencar <rvsa@cesar.org.br>
2016-08-16 10:20:59 -0300

    spi: Adds spi init for arduino.
    This patch adds the function to init spi in arduino.

--- src/spi/spi_arduino.c
+++ src/spi/spi_arduino.c
@@ -1,155 +1,53 @@
 /*
- * Copyright (c) 2010 by Cristian Maglie <c.maglie@arduino.cc>
- * Copyright (c) 2014 by Paul Stoffregen <paul@pjrc.com> (Transaction API)
- * Copyright (c) 2014 by Matthijs Kooijman <matthijs@stdin.nl> (SPISettings AVR)
- * Copyright (c) 2014 by Andrew J. Kroll <xxxajk@gmail.com> (atomicity fixes)
- * SPI Master library for arduino.
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
- * published by the Free Software Foundation.
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
  */
-#include "knot_spi.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "spi.h"
 
-#define ARDUINO
-#ifdef ARDUINO
+#define CSN		2
+#define MOSI	3
+#define MISO	4
+#define SCK		5
 
-#include <Arduino.h>
+static bool	m_init = false;
 
-static bool m_initialized = false;
-static uint8_t m_interruptMode = 0;
-static uint8_t m_interruptMask = 0;
-static uint8_t m_interruptSave = 0;
-
-uint8_t knot_spi_initialize()
+int spi_init(const char *dev)
 {
-	if (initialized) {
-		return 1;
+	if (!m_init) {
+		m_init = true;
+
+		//Put CSN HIGH
+		PORTB |= (1 << CSN);
+
+		//CSN as output
+		DDRB |= (1 << CSN);
+
+		//Enable SPI and set as master
+		SPCR |= (1 << SPE) | (1 << MSTR);
+
+		//MISO as input, MOSI and SCK as output
+		DDRB &= ~(1 << MISO);
+		DDRB |= (1 << MOSI);
+		DDRB |= (1 << SCK);
+
+		//SPI mode 0
+		SPCR &= ~(1 << CPOL);
+		SPCR &= ~(1 << CPHA);
+
+		//vel fosc/16 = 1MHz
+		SPCR &= ~(1 << SPI2X);
+		SPCR &= ~(1 << SPR1);
+		SPCR |= (1 << SPR0);
 	}
-
-	initialized = true;
-
-	uint8_t sreg = SREG;
-	noInterrupts(); // Protect from a scheduler and prevent transactionBegin
-
-	// Set it high (to enable the internal pull-up resistor)
-	if(!(*portModeRegister(digitalPinToPort(SS)) & digitalPinToBitMask(SS))) {
-		digitalWrite(SS, HIGH);
-	}
-
-	pinMode(SCK, OUTPUT);
-	pinMode(MOSI, OUTPUT);
-	pinMode(SS, OUTPUT);
-
-	// Warning: if the SS pin ever becomes a LOW INPUT then SPI
-	// automatically switches to Slave, so the data direction of
-	// the SS pin MUST be kept as OUTPUT.
-	SPCR |= _BV(MSTR);
-	SPCR |= _BV(SPE);
-
-	SREG = sreg;
-
 	return 0;
 }
 
-void SPIClass::end()
-{
-	if(initialized)
-	{
-		initialized = false;
-
-		uint8_t sreg = SREG;
-		noInterrupts();
-
-		SPCR &= ~_BV(SPE);
-		m_interruptMode = 0;
-		SREG = sreg;
-}
-
-#ifdef INT0
-#define INT0_MASK  (1<<INT0)
-#endif
-#ifdef INT1
-#define INT1_MASK  (1<<INT1)
-#endif
-
-void InterruptON(uint8_t intx)
-{
-	uint8_t mask = 0;
-	uint8_t sreg = SREG;
-
-	noInterrupts();
-	switch(intx) {
-	case INT0:
-		mask = 1 << INT0;
-		break;
-	case INT1:
-		mask = 1 << INT1;
-		break;
-	default:
-		interruptMode = 2;
-		break;
-	}
-	m_interruptMask |= mask;
-	if(m_interruptMode == 0) {
-		m_interruptMode = 1;
-	}
-
-	SREG = sreg;
-}
-
-void InterruptOFF(uint8_t intx)
-{
-	if (m_interruptMode != 2) {
-		uint8_t mask = 0;
-		uint8_t sreg = SREG;
-
-		noInterrupts();
-		switch(intx) {
-		case INT0:
-			mask = 1<<INT0 ;
-			break;
-		case INT1:
-			mask = 1<<INT1;
-			break;
-		}
-		m_interruptMask &= ~mask;
-		if(m_interruptMask == 0) {
-			m_interruptMode = 0;
-		}
-
-		SREG = sreg;
-	}
-}
-
-// Set exclusive access to the SPI bus and configuring the correct settings.
-static void beginTransaction(SPISettings settings)
-{
-  if (interruptMode > 0) {
-    uint8_t sreg = SREG;
-    noInterrupts();
-
-    if (interruptMode == 1) {
-      interruptSave = EIMSK;
-      EIMSK &= ~interruptMask;
-      SREG = sreg;
-    } else
-    {
-      interruptSave = sreg;
-    }
-  }
-
-  #ifdef SPI_TRANSACTION_MISMATCH_LED
-  if (inTransactionFlag) {
-    pinMode(SPI_TRANSACTION_MISMATCH_LED, OUTPUT);
-    digitalWrite(SPI_TRANSACTION_MISMATCH_LED, HIGH);
-  }
-  inTransactionFlag = 1;
-  #endif
-
-  SPCR = settings.spcr;
-  SPSR = settings.spsr;
-}
-
-#endif
